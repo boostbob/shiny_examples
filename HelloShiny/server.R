39a46d46857ce6e3da105553aeef643adc870be7
Bob Wang <wangbo@mainbo.com>
2016-06-26 12:30:50 +0800

    add(debug): options & browser debugging statment

--- HelloShiny/server.R
+++ HelloShiny/server.R
@@ -5,9 +5,23 @@ mpgData <- mtcars
 mpgData$am <- factor(mpgData$am, labels = c("自动", "手动"))
 
 shinyServer(function(input, output) {
+    # 调试信息
+    cat('Shiny is really cool')
+
+    # 调试语句，只有交互式运行才会中断
+    # browser()
+    
+    # 选择am的时候中断运行，进入调试器
+    # browser(expr = identical(input$variable, "am"))
+
+    # 错误发生的时候自动进入浏览器
+    options(error = browser)
+    # 设置recover为错误处理器，打印堆栈和调用列表 
+    options(error = recover)
+
     # 该输入被包装成响应式的表达式，引用他的表达式也会成为响应式的
     formulaText <- reactive({
-        paste("mpg ~", input$variable)
+        paste("mpg ~ ", input$variable)
     })
 
     # 需要使用 renderText 包装才能使响应式的
