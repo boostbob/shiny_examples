bd9f520830ce5d583ecb7b7964a0e430aa1d611e
Bob Wang <wangbo@mainbo.com>
2016-06-26 12:04:18 +0800

    step(reactive expression): using reactive & renderText & renderPlot to interactive with user

--- HelloShiny/server.R
+++ HelloShiny/server.R
@@ -1,5 +1,26 @@
 library(shiny)
+library(datasets)
+
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("自动", "手动"))
 
 shinyServer(function(input, output) {
-    #
+    # 该输入被包装成响应式的表达式，引用他的表达式也会成为响应式的
+    formulaText <- reactive({
+        paste("mpg ~", input$variable)
+    })
+
+    # 需要使用 renderText 包装才能使响应式的
+    # 不能直接赋值
+    output$caption <- renderText({
+        formulaText()
+    })
+
+    # 需要使用 renderPlot 包装才能使响应式的
+    # 不能直接赋值
+    output$mpgPlot <- renderPlot({
+        boxplot(as.formula(formulaText()),
+                data = mpgData,
+                outline = input$outliers)
+    })
 })
--- HelloShiny/ui.R
+++ HelloShiny/ui.R
@@ -1,11 +1,21 @@
 library(shiny)
 
 shinyUI(pageWithSidebar (
-    headerPanel("Hello Shiny"),
+    headerPanel("Shiny 第一个应用"),
 
     # side section
-    sidebarPanel(),
+    sidebarPanel(
+        selectInput('variable', "变量", 
+                    list("Cylinders" = "cyl",
+                         "Transmission" = "am",
+                         "Gears" = "gear")),
+
+        checkboxInput("outliers", "显示异常值", FALSE)
+    ),
     
     # main section
-    mainPanel()
+    mainPanel(
+        h3(textOutput('caption')),
+        plotOutput("mpgPlot")
+    )
 ))
