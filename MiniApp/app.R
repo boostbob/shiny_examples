2d66634c595421b7a976a74dc17d0c3a94dff93c
Bob Wang <wangbo@mainbo.com>
2016-07-04 14:29:36 +0800

    add(reactive): create isolate reaction using isolate function

--- MiniApp/app.R
+++ MiniApp/app.R
@@ -1,9 +1,13 @@
-# it's runnable ;-)
+# it's runnable
 # but nothing to display
 
 library('shiny')
 
 server <- shinyServer(function(input, output, session) {})
+#server <- function(input, output, session) {}
+#server <- function(input, output) {}
+
 ui <- shinyUI(basicPage(titlePanel('Mini Shiny App')))
+#ui <- basicPage()
 
 shinyApp(ui = ui, server = server)
